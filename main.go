5a7251050a207fcebdc8221eaac837dc609ed3b8
44smkn <jrywm121@gmail.com>
2019-05-23 13:52:53 +0900

    add process that call the parse log function

--- main.go
+++ main.go
@@ -1,36 +1,58 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
-	"goriky/infrastructure/client"
 	"goriky/accesslog/apache"
-	"io/ioutil"
+	"goriky/infrastructure/client"
+	"io"
 	"log"
-	"strings"
+	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
-	b, err := ioutil.ReadFile("sample.log")
+	f, err := os.Open("sample.log")
 	if err != nil {
 		log.Fatalln("reading file is failed because of " + err.Error())
 	}
-	accessLogs := strings.Split(string(b), "\n")
+	defer f.Close()
+
+	format := ""
+	p := apache.NewParser(format)
+	c := client.New()
+	buf := bufio.NewReaderSize(f, 4096) // may change buffer size...?
+	reqs := make([]*http.Request, 10)
+	for line := ""; err == nil; line, err = buf.ReadString('\n') {
+		req, err := p.Parse(line)
+		if err != nil {
+			log.Fatalln("To gain the line is failed because of " + err.Error())
+		}
+		reqs = append(reqs, req)
+	}
+	if err != io.EOF {
+		log.Fatalln("reading line is failed." + err.Error())
+	}
+
+	client := client.New()
 	reqPerMinute := 10
-	client, _ := client.New("http://hogehoge.com")
-	apacheLog := &apache.LogParser{}
+
+	for _, req := range reqs {
+		ctx := context.Background()
+		c.SendRequest(ctx, req)
+	}
 
 	t := time.NewTicker(time.Duration(60/reqPerMinute) * time.Second)
 	defer t.Stop()
 	i := 0
 	for range t.C {
-		if i <= len(accessLogs) {
+		if i <= len(reqs) {
 			i = 0
 		}
-		req, _ := apacheLog.Parse(accessLogs[i])
 		ctx := context.Background()
-		res, _ := client.SendRequest(ctx, req)
+		res, _ := client.SendRequest(ctx, reqs[i])
 		fmt.Println(res)
 	}
 }
